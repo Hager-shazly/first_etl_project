e99b7fe35341968f60912584506048a0387f7f56
Hagar shazly <hagershazley295@gmail.com>
2025-05-22 10:58:32 +0300

    addd loading the data

--- etl.py
+++ etl.py
@@ -10,3 +10,5 @@ df.fillna({"name": "Unknown", "age": df["age"].median(), "purchase_amount": 0}, 
 
 # Convert purchase date to uniform format
 df["purchasedate"] = pd.to_datetime(df["purchasedate"], errors="coerce")
+
+df.to_csv("final_data.csv", index=False)
