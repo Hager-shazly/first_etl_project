833c4d204226baf61d2ac27f8a927e56d5c4037f
Hagar shazly <hagershazley295@gmail.com>
2025-05-22 10:56:45 +0300

    added transformes to the csv file

--- etl.py
+++ etl.py
@@ -2,3 +2,11 @@ import pandas as pd
 
 # Load raw data
 df = pd.read_csv("raw_data.csv")
+
+df.columns = [col.lower().replace(" ", "_") for col in df.columns]
+
+# Handle missing values
+df.fillna({"name": "Unknown", "age": df["age"].median(), "purchase_amount": 0}, inplace=True)
+
+# Convert purchase date to uniform format
+df["purchasedate"] = pd.to_datetime(df["purchasedate"], errors="coerce")
