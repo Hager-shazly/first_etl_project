f0e8ba97a7a1c2c0b6a8595e4bb7b1ca8cfad658
Hagar shazly <hagershazley295@gmail.com>
2025-05-24 11:44:45 +0300

    modified etl file

--- etl.py
+++ etl.py
@@ -11,4 +11,4 @@ df.fillna({"name": "Unknown", "age": df["age"].median(), "purchase_amount": 0}, 
 # Convert purchase date to uniform format
 df["purchasedate"] = pd.to_datetime(df["purchasedate"], errors="coerce")
 
-df.to_csv("final_data.csv", index=False)
+df.to_json("final_data.csv", index=False)
